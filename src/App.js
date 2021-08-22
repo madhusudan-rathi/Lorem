3a53f7108b9c6d6355e269b7c158fac35b538635
iammadhusudanrathi <iammadhusudanrathi@gmail.com>
2021-08-22 21:54:08 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import data from './data';
+
+function App() {
+	const [count, setCount] = useState(0);
+	const [text, setText] = useState([]);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		let amount = parseInt(count);
+		if(count > 8) {
+			amount = 8;
+		}
+		setText(data.slice(0, amount));
+	}
+	return (
+		<React.Fragment>
+            <section className="section-center">
+	            <h3>Tired of boring lorem ipsum?</h3>
+	            <form className="lorem-form" onSubmit={handleSubmit}>
+		            <label htmlFor="amount">Paragraphs:</label>
+		            <input
+			            type="number"
+			            name="amount"
+			            id="amount"
+			            min="0"
+			            value={count}
+			            onChange={(e) => {setCount(e.target.value)}}
+		            />
+		            <button className="btn" type="submit">Generate</button>
+	            </form>
+	            <article className="lorem-text">
+		            {
+		            	text.map((item, index) => {
+				            return (
+					            <p key={index}>{item}</p>
+				            );
+			            })
+		            };
+	            </article>
+            </section>
+		</React.Fragment>
+    );
+}
+
+export default App;
